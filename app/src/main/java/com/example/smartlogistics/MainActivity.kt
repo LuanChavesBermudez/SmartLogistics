a3d5752ca6cebd790d62c0d0aed1e94ac51d31b4
AnthonySV27 <anthonysequeiravargas153@gmail.com>
2026-08-05 16:03:19 -0600

    Se añadió el botón y cambió el nombre a las ventanas

--- app/src/main/java/com/example/smartlogistics/MainActivity.kt
+++ app/src/main/java/com/example/smartlogistics/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,11 +19,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import androidx.compose.ui.unit.dp
 import com.example.smartlogistics.ui.theme.SmartLogisticsTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,7 +44,7 @@ class MainActivity : ComponentActivity() {
 @PreviewScreenSizes
 @Composable
 fun SmartLogisticsApp() {
-    var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.HOME) }
+    var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.TEMPERATURA) }
 
     NavigationSuiteScaffold(
         navigationSuiteItems = {
@@ -57,11 +64,41 @@ fun SmartLogisticsApp() {
         }
     ) {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Greeting(
-                name = "Android",
-                modifier = Modifier.padding(innerPadding)
-            )
+            val modifier = Modifier.padding(innerPadding)
+            when (currentDestination) {
+                AppDestinations.TEMPERATURA -> TemperaturaScreen(modifier)
+                AppDestinations.HISTORIAL -> HistorialScreen(modifier)
+            }
         }
+    }
+}
+
+@Composable
+fun TemperaturaScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = { /* TODO: Implementar acción */ },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Blue),
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text(text = "TEMPERATURA ACTUAL")
+        }
+    }
+}
+
+@Composable
+fun HistorialScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Pantalla de Historial")
+
     }
 }
 
@@ -69,23 +106,22 @@ enum class AppDestinations(
     val label: String,
     val icon: Int,
 ) {
-    HOME("Home", R.drawable.ic_home),
-    FAVORITES("Favorites", R.drawable.ic_favorite),
-    PROFILE("Profile", R.drawable.ic_account_box),
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    TEMPERATURA("Temperatura", R.drawable.ic_home),
+    HISTORIAL("Historial", R.drawable.ic_favorite),
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TemperaturaScreenPreview() {
     SmartLogisticsTheme {
-        Greeting("Android")
+        TemperaturaScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SmartLogisticsAppPreview() {
+    SmartLogisticsTheme {
+        SmartLogisticsApp()
     }
 }
